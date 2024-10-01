c13f020f22b47910fc1f83d720d5e10b8c6d105d
JanTHorstmann <janhorstmann@yahoo.de>
2024-10-01 05:51:02 +0200

    convert video

--- videoflix/content/signals.py
+++ videoflix/content/signals.py
@@ -1,17 +1,30 @@
 from .models import Video
+from content.tasks import convert_480p, convert_720p, convert_1080p
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 import os
+
 
 @receiver(post_save, sender=Video)
 def video_post_save(sender, instance, created, **kwargs):
     print('Video saved!')
     if created:
-        print('new Video created')
+        convert_480p(instance.video_file.path)
+        convert_720p(instance.video_file.path)
+        convert_1080p(instance.video_file.path)
 
 @receiver(post_delete, sender=Video)
 def video_post_delete(sender, instance, using, origin, **kwargs):
     if instance.video_file:
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
+
+            if os.path.isfile(instance.video_file.path[:-4]+'_480p.mp4'):
+                os.remove(instance.video_file.path[:-4]+'_480p.mp4')
+
+            if os.path.isfile(instance.video_file.path[:-4]+'_720p.mp4'):
+                os.remove(instance.video_file.path[:-4]+'_720p.mp4')
+
+            if os.path.isfile(instance.video_file.path[:-4]+'_1080p.mp4'):
+                os.remove(instance.video_file.path[:-4]+'_1080p.mp4')
             print('File deleted:', instance.video_file.path)
