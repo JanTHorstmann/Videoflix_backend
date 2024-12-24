8dc9f8ed7750c36813fd59e110795e7b9ff6a953
JanTHorstmann <janhorstmann@yahoo.de>
2024-12-24 11:46:04 +0100

    update VideoProgressSerializer

--- videoflix/content/serializers.py
+++ videoflix/content/serializers.py
@@ -7,7 +7,8 @@ class VideoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class VideoProgressSerializer(serializers.ModelSerializer):
+    video_id = serializers.IntegerField(source='video.id', read_only=True)
     class Meta:
         model = VideoProgress
-        fields = ['played_time']  # Nur `played_time` als Eingabefeld zulassen
+        fields = ['video_id', 'played_time']  # Nur `played_time` als Eingabefeld zulassen
         read_only_fields = ['user', 'video', 'last_updated']  # Diese Felder sind nur lesbar
